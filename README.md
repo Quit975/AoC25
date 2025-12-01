57dd81d327f00fd31b0a47cb28a436662e1f9bf2
Quit975 <plushquotient@gmail.com>
2025-12-01 07:44:30 +0100

    Repo init

---
+++ README.md
@@ -0,0 +1,3 @@
+# AoC25
+
+Yeah, I'll probably do just a few of those part 3 🤷‍♂️
---
+++ day_1.lua
@@ -0,0 +1,36 @@
+require "utils/strings"
+require "utils/sets"
+
+local input_file, error_msg = io.open("inputs\\day1input.txt", "r");
+if not input_file then
+    print(error_msg);
+    return;
+end
+
+function SolveFirstPart()
+    local start_time = os.clock();
+    local solution = 0;
+
+    local end_time = os.clock();
+    local final_time = end_time - start_time;
+   
+    print("First part solution!");
+    print(string.format("The solution is %d", solution));
+    print(string.format("The solution took %f seconds", final_time));
+end
+
+
+function SolveSecondPart()
+    local start_time = os.clock();
+    local solution = 0;
+
+    local end_time = os.clock();
+    local final_time = end_time - start_time;
+
+    print("Second part solution!");
+    print(string.format("The solution is %d", solution));
+    print(string.format("The solution took %f seconds", final_time));
+end
+
+SolveFirstPart();
+SolveSecondPart();
