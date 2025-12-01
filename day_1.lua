3a6914795544aaf21250381e4962d2887347e49a
Quitt <plushquotient@gmail.com>
2025-12-01 17:02:03 +0100

    Day 1 Part 2 wip not solved yet

--- day_1.lua
+++ day_1.lua
@@ -22,7 +22,7 @@ function SolveFirstPart()
 
         local actual_turn_amount = turn_amount % (max_dial + 1) ;
         if turn_to_left then
-            actual_turn_amount = - actual_turn_amount;
+            actual_turn_amount = -actual_turn_amount;
         end
 
         current_dial = current_dial + actual_turn_amount;
@@ -36,6 +36,7 @@ function SolveFirstPart()
             solution = solution + 1;
         end
     end
+    input_file:seek("set", 0);
 
     local end_time = os.clock();
     local final_time = end_time - start_time;
@@ -49,6 +50,38 @@ end
 function SolveSecondPart()
     local start_time = os.clock();
     local solution = 0;
+
+    local current_dial = 50;
+    local max_dial = 99;
+
+    for line in input_file:lines() do
+        local direction_string = string.sub(line, 1, 1);
+        local turn_to_left = direction_string == "L" and true or false;
+        local turn_amount = tonumber(string.sub(line, 2));
+        assert(type(turn_amount) == "number");
+
+        local overflow_turns = math.floor(turn_amount / (max_dial + 1));
+        solution = solution + overflow_turns;
+
+        local actual_turn_amount = turn_amount % (max_dial + 1) ;
+        if turn_to_left then
+            actual_turn_amount = -actual_turn_amount;
+        end
+
+        current_dial = current_dial + actual_turn_amount;
+        if current_dial < 0 then
+            current_dial = (max_dial + 1) + current_dial;
+            solution = solution + 1;
+        elseif current_dial > max_dial then
+            current_dial = current_dial - (max_dial + 1);
+            solution = solution + 1;
+        end
+
+        if current_dial == 0 then
+            solution = solution + 1;
+        end
+    end
+    input_file:seek("set", 0);
 
     local end_time = os.clock();
     local final_time = end_time - start_time;
