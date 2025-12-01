6495854b8693dc02ca50508861e917d5154414be
Quitt <plushquotient@gmail.com>
2025-12-01 16:48:46 +0100

    Day 1 Part 1 solution

--- day_1.lua
+++ day_1.lua
@@ -11,6 +11,32 @@ function SolveFirstPart()
     local start_time = os.clock();
     local solution = 0;
 
+    local current_dial = 50;
+    local max_dial = 99;
+
+    for line in input_file:lines() do
+        local direction_string = string.sub(line, 1, 1);
+        local turn_to_left = direction_string == "L" and true or false;
+        local turn_amount = tonumber(string.sub(line, 2));
+        assert(type(turn_amount) == "number");
+
+        local actual_turn_amount = turn_amount % (max_dial + 1) ;
+        if turn_to_left then
+            actual_turn_amount = - actual_turn_amount;
+        end
+
+        current_dial = current_dial + actual_turn_amount;
+        if current_dial < 0 then
+            current_dial = (max_dial + 1) + current_dial;
+        elseif current_dial > max_dial then
+            current_dial = current_dial - (max_dial + 1);
+        end
+
+        if current_dial == 0 then
+            solution = solution + 1;
+        end
+    end
+
     local end_time = os.clock();
     local final_time = end_time - start_time;
    
