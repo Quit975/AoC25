5d55807a38e1b6b87b326395d5119819c5edc576
Quit975 <plushquotient@gmail.com>
2025-12-02 21:12:46 +0100

    Day 2 Part 1 done

--- day_2.lua
+++ day_2.lua
@@ -7,22 +7,69 @@ if not input_file then
     return;
 end
 
+function MakeDoubleNumber(number)
+    assert(number and type(number) == "number");
+    local number_as_string = tostring(number);
+    local double_number_as_string = number_as_string .. number_as_string;
+    return tonumber(double_number_as_string);
+end
+
+function GetHalfNumber(number)
+    assert(number and type(number) == "number");
+    local number_as_string = tostring(number);
+    local number_length = #number_as_string;
+    assert((number_length % 2) == 0);
+    local number_half_idx = number_length / 2;
+    local half_number_as_string = string.sub(number_as_string, 1, number_half_idx);
+    return tonumber(half_number_as_string);
+end
+
+function GetMaxNumberOfLength(number_length)
+    assert(type(number_length) == "number" and number_length > 0);
+    local string_num = "";
+    for i = 1, number_length do
+        string_num = string_num .. "9";
+    end
+    return tonumber(string_num);
+end
+
 function SolveFirstPart()
     local start_time = os.clock();
     local solution = 0;
 
-    local CheckRange = function(left, right, current_solution)
-        
-        local left_len = math.floor(math.log(left, 10)) + 1;
-        local right_len = math.floor(math.log(right, 10)) + 1;
+    local CheckRange = function(left_number, right_number, current_solution)
+        assert(type(left_number) == "number" and type(right_number) == "number" and type(current_solution) == "number");
+
+        local left_len = math.floor(math.log(left_number, 10)) + 1;
+        local right_len = math.floor(math.log(right_number, 10)) + 1;
+        local from_number = left_number;
+        local to_number = right_number;
 
         if left_len == right_len then
             if left_len % 2 ~= 0 then
                 return current_solution;
             end
+        else
+            if left_len % 2 == 0 then
+                to_number = GetMaxNumberOfLength(left_len);
+            else
+                from_number = GetMaxNumberOfLength(left_len) + 1;
+            end
         end
 
-        print(string.format("From %d (%d) to %d (%d)", left, left_len, right, right_len));
+        local left_half_num = GetHalfNumber(from_number);
+        local left_double_num = MakeDoubleNumber(left_half_num);
+
+        if left_double_num < from_number then
+            left_half_num = left_half_num + 1;
+            left_double_num = MakeDoubleNumber(left_half_num);
+        end
+
+        while left_double_num <= to_number do
+            current_solution = current_solution + left_double_num;
+            left_half_num = left_half_num + 1;
+            left_double_num = MakeDoubleNumber(left_half_num);
+        end
 
         return current_solution;
     end
