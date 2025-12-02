9e42f49f1bf7b92ed280e609754f72c7f47f1f60
Quitt <plushquotient@gmail.com>
2025-12-02 17:26:01 +0100

    Day 2 Part 1 some stuff dunno, still wip

--- day_2.lua
+++ day_2.lua
@@ -10,6 +10,22 @@ end
 function SolveFirstPart()
     local start_time = os.clock();
     local solution = 0;
+
+    local CheckRange = function(left, right, current_solution)
+        
+        local left_len = math.floor(math.log(left, 10)) + 1;
+        local right_len = math.floor(math.log(right, 10)) + 1;
+
+        if left_len == right_len then
+            if left_len % 2 ~= 0 then
+                return current_solution;
+            end
+        end
+
+        print(string.format("From %d (%d) to %d (%d)", left, left_len, right, right_len));
+
+        return current_solution;
+    end
     
     for line in input_file:lines() do
         local current_span, remaining_string = SplitString(line, ",");
@@ -18,15 +34,16 @@ function SolveFirstPart()
             assert(left and right);
             assert(right > left);
 
-            print(string.format("From %d to %d", left, right));
-
+            solution = CheckRange(left, right, solution);
+            
             current_span, remaining_string = SplitString(remaining_string, ",");
         end
 
         local left, right = SplitStringAsNumbers(current_span, "-");
         assert(left and right);
         assert(right > left);
-        print(string.format("From %d to %d", left, right));
+
+        solution = CheckRange(left, right, solution);
     end
     input_file:seek("set", 0);
 
